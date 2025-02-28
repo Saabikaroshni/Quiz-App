a85bd87ea046bbfbe0d90d353de5ea88885b52e5
Saabika Roshni S <saabikaroshni@gmail.com>
2025-02-28 09:56:56 +0530

    backend created

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mdb = require("mongoose");
+const dotenv=require('dotenv')
+const app = express();
+const PORT = 3001;
+dotenv.config()
+mdb
+  .connect("mongodb://127.0.0.1:27017/Trivia-app")
+  .then(() => {
+    console.log("MBD sucess");
+  })
+  .catch((err) => {
+    console.log("cheack you string", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("<h1>welcome back<h1>");
+});
+// app.get("/static", (req, res) => {
+//   res.sendFile(
+//     "C:\\Users\\joans\\OneDrive\\Documents\\desktop\\mern\\html_css\\index.html"
+//   );
+// });
+
+app.listen(PORT, () => console.log("server started successfully"));
